0642ee9c6ee8f9caa5e85488d06120fe25ec2b3f
Danila Maltsau <165064438+sajicklevo@users.noreply.github.com>
2024-04-29 10:48:25 +0300

    задача 1 .py
    Работа с модулем os
    Есть папка, в которой лежат файлы с разными расширениями.
    Программа должна:
    ● Вывести имя вашей ОС
    ● Вывести путь до папки, в которой вы находитесь
    ● Рассортировать файлы по расширениям, например, для
    текстовых файлов создается папка, в неё перемещаются
    все файлы с расширением .txt, то же самое для остальных
    расширений
    ● После рассортировки выводится сообщение типа «в папке
    с текстовыми файлами перемещено 5 файлов, их
    суммарный размер - 50 гигабайт»
    ● Как минимум один файл в любой из получившихся
    поддиректорий переименовать. Сделать вывод
    сообщения типа «Файл data.txt был переименован в
    some_data.txt»
    ● Программа должна быть кроссплатформенной – никаких
    хардкодов с именем диска и слэшами.

--- задача 1 .py
+++ задача 1 .py
@@ -1,8 +1,38 @@
-
+import os
+import random
 import string
-import secrets
+
+osname = os.name
+print('Имя вашей операционной системы:', osname)
+
+path = os.getcwd()
+print('Путь до папки:', path)
+
+extensions = set()
+while True:
+    extension = input('Введите расширение файла (или введите "exit" для завершения): ')
+    if extension == 'exit':
+        break
+    extensions.add(extension)
+print('Расширения файлов:', extensions)
+
+for file_name in os.listdir(path):
+    if os.path.isfile(os.path.join(path, file_name)):
+        file_extension = file_name.split('.')[-1]
+        if file_extension in extensions:
+            folder_name = file_extension.upper()
+            if not os.path.exists(os.path.join(path, folder_name)):
+                os.makedirs(os.path.join(path, folder_name))
+            new_file_path = os.path.join(path, folder_name, file_name)
+            os.rename(os.path.join(path, file_name), new_file_path)
+print('Файлы были перемещены в папки с соответствующими расширениями')
+
+for ext in os.listdir(path):
+    if os.path.isdir(os.path.join(path, ext)):
+        files_moved = len(os.listdir(os.path.join(path, ext)))
+        total_size = sum(os.path.getsize(os.path.join(path, ext, file)) for file in os.listdir(os.path.join(path, ext)))
+        print(f'В папке с файлами {ext} перемещено {files_moved} файлов, их суммарный размер - {total_size} байт')
 
 def random_string(length):
     letters = string.ascii_letters
-    return ''.join(secrets.choice(letters) for _ in range(length))
-print(random_string(10))
+    return ''.join(random.choice(letters) for i in range(length))
